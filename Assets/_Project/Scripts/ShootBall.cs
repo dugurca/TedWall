afd02c2ec46ffd6010682033dbeed59d830ffbb8
Deniz Ugurca <dugurca@gmail.com>
2016-11-11 00:07:38 +0300

    week1

---
+++ Assets/_Project/Scripts/ShootBall.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts
+{
+    public class ShootBall : MonoBehaviour
+    {
+        public GameObject BallPrefab;
+
+        // Use this for initialization
+        void Start () {
+
+        }
+
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+
+        void Shoot()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject ball = Instantiate(BallPrefab);
+                ball.transform.position = transform.position;
+                Vector3 forceDir = hit.point - transform.position;
+                ball.GetComponent<Rigidbody>().velocity = forceDir * 5f;
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/WallGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts
+{
+    public class WallGenerator : MonoBehaviour
+    {
+        public GameObject Brick;
+
+        void Start () {
+            GenerateWall(20, 40);
+        }
+
+        void GenerateWall(int numRows, int numCols)
+        {
+            Vector3 startPos = new Vector3(-numCols, 0.5f, numCols);
+            for (int row = 0; row < numRows; row++)
+            {
+                Vector3 p = row%2 == 0 ? Vector3.left : Vector3.zero;
+                for (int col = 0; col < numCols; col++)
+                {
+                    Vector3 pos = startPos + new Vector3(col * 3, row, 0) + p;
+                    Instantiate(Brick, pos, Quaternion.identity);
+                }
+            }
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	
+        }
+    }
+}
